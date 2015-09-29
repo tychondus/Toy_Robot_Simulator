e76c49570189e8e71ad75938eef64c13af17d11e
Sarma <sarma.palaniandy@gmail.com>
2015-09-29 15:14:05 +0800

    Implemented test to check that x and y boundary when multiplied equal size

--- lib/board.rb
+++ lib/board.rb
@@ -5,6 +5,18 @@ class Board
     @y_boundary = Math.sqrt(@size).to_int
   end
 
+  def x_boundary
+    @x_boundary
+  end
+
+  def y_boundary
+    @y_boundary
+  end
+
+  def size
+    @size
+  end
+
   def is_valid_X(value)
     return value < @x_boundary ? true : false
   end
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -9,10 +9,16 @@ RSpec.describe Board, "#check" do
   end
   
   context "new y value not exceeding the prescribed dimension" do
-    it "copmpare the given value with the limit for y co-ordinate" do
+    it "compare the given value with the limit for y co-ordinate" do
       board = Board.new
       expect(board.is_valid_Y(6)).to eq false
     end
   end
 
+  context "X and Y value to ensure that they match size" do
+    it "multiply x_boundary and y_boundary and verify it with size" do
+      board = Board.new
+      expect(board.x_boundary * board.y_boundary).to eq board.size
+    end
+  end
 end
