fa3ccf9b86970bdda39f45bce16e057cb6789085
Sarma <sarma.palaniandy@gmail.com>
2015-09-29 14:57:00 +0800

    Added test to ensure that is_valid_Y is functioning properly

--- lib/board.rb
+++ lib/board.rb
@@ -1,3 +1,15 @@
 class Board
+  def initialize
+    @size = 25
+    @x_boundary = Math.sqrt(@size).to_int
+    @y_boundary = Math.sqrt(@size).to_int
+  end
 
+  def is_valid_X(value)
+    return value < @x_boundary ? true : false
+  end
+
+  def is_valid_Y(value)
+    return value < @y_boundary ? true : false
+  end
 end
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -1,11 +1,18 @@
 require 'board'
 
-RSpec.describe Board, "#something" do
-  context "something" do
-    it "" do
+RSpec.describe Board, "#check" do
+  context "new x value not exceeding the prescribed dimension" do
+    it "compare the given value with the limit for x co-ordinate" do
       board = Board.new
-      board.boundary = 5
       expect(board.is_valid_X(6)).to eq false
     end
   end
+  
+  context "new y value not exceeding the prescribed dimension" do
+    it "copmpare the given value with the limit for y co-ordinate" do
+      board = Board.new
+      expect(board.is_valid_Y(6)).to eq false
+    end
+  end
+
 end
