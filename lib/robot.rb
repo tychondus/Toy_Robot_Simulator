98f3ce3b3630d39ce71fe0b152d375f6bb849087
Sarma <sarma.palaniandy@gmail.com>
2015-09-30 11:46:15 +0800

    created a private method called orientation_index. To centralize the find_index call

--- lib/robot.rb
+++ lib/robot.rb
@@ -21,19 +21,24 @@ class Robot
   end
 
   def is_valid_direction(direction)
-    index = @orientations.find_index(direction.upcase)
-    index != nil ? true : false
+    orientation_index(direction) != nil ? true : false
   end 
   
   def rotate_left
-    index = @orientations.find_index(direction.upcase)
+    index = orientation_index(direction) 
     index == 0 ? index = @orientations.length - 1 : index -= 1
     @direction = @orientations[index]
   end
 
   def rotate_right
-    index = @orientations.find_index(direction.upcase)
+    index = orientation_index(direction)
     index == @orientations.length - 1 ? index = 0 : index += 1
     @direction = @orientations[index]
   end
+  
+  def orientation_index(direction)
+    @orientations.find_index(direction.upcase)
+  end
+
+  private :orientation_index
 end
