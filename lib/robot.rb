c09a081a809cc0195c5680bb0e643c4d7a78a904
Sarma <sarma.palaniandy@gmail.com>
2015-09-30 08:31:11 +0800

    added some range and value check during class initialisation

--- lib/robot.rb
+++ lib/robot.rb
@@ -1,9 +1,23 @@
 class Robot
-  def initialize(x = 0, y = 0, direction = 'N')
-    @loc_x = x
-    @loc_y = y
-    @direction = direction
-    @orientations = [ 'N', 'E', 'S', 'W' ]
+  DEFAULT_LOC = 0
+  DEFAULT_DIR = 'N'
+  def initialize(x = DEFAULT_LOC, y = DEFAULT_LOC, direction = DEFAULT_DIR)
+    @orientations = ['N', 'E', 'S', 'W' ]
+    x < 0 ? @loc_x = DEFAULT_LOC : @loc_x = x
+    y < 0 ? @loc_y = DEFAULT_LOC : @loc_y = y
+    is_valid_direction(direction) ? @direction = direction : @direction = DEFAULT_DIR
+  end
+
+  def loc_x
+    @loc_x
+  end
+
+  def loc_y
+    @loc_y
+  end
+
+  def direction
+    @direction
   end
 
   def is_valid_direction(direction)
--- spec/robot_spec.rb
+++ spec/robot_spec.rb
@@ -1,6 +1,23 @@
 require 'robot'
 
 RSpec.describe Robot, "#check" do
+  context "that only non negative value is accepted for x and y value during class initialiasation" do
+    it "insert negative value of x and y" do
+      robot = Robot.new(-1, -1)
+      expect(robot.loc_x).to eq 0
+      expect(robot.loc_y).to eq 0
+    end
+  end
+
+  context "that only valid entry is accepted for orientation during class initialisation" do
+    it "insert a value that is not N,S,E or W it should default to 'N'" do
+      robot = Robot.new(2, 2, 'T')
+      expect(robot.loc_x).to eq 2
+      expect(robot.loc_y).to eq 2
+      expect(robot.direction).to eq 'N'
+    end
+  end
+
   context "that invalid entries are not accepted for orientation" do
     it "attempt to enter an invalid character" do
       robot = Robot.new
